323b68f323862b4b22e3d12df007d760979f35ca
nathan <nathan+dev@electriccoin.co>
2020-03-03 15:47:47 -0800

    Refactor w/ private chunk-writing methods.

--- src/phase/indenter.rs
+++ src/phase/indenter.rs
@@ -1,46 +1,77 @@
 #[cfg(test)]
 mod tests;
-mod writecounter;
 
 use std::io::{Result, Write};
 
-pub struct Indenter<W>(W);
+pub struct Indenter<W> {
+    f: W,
+    linestart: bool,
+}
 
 impl<W> From<W> for Indenter<W> {
     fn from(f: W) -> Indenter<W> {
-        Indenter(f)
+        Indenter {
+            f: f,
+            linestart: true,
+        }
     }
 }
 
 impl<W: Write> Write for Indenter<W> {
     fn write(&mut self, buf: &[u8]) -> Result<usize> {
-        use self::writecounter::WriteCounter;
+        let mut written = 0;
+        let mut chunks = buf.split(|&c| c == b'\n').fuse();
+        let mut ochunk = chunks.next();
 
-        let mut wc = WriteCounter::new(&mut self.0);
-        let mut first = true;
-
-        for chunk in buf.split(|&c| c == b'\n') {
-            if first {
-                first = false;
-            } else {
-                wc.write_counted("\n")?;
-                wc.write_uncounted("| ")?;
-            }
-
-            wc.write_counted(chunk)?;
+        for nextchunk in chunks {
+            let chunk = ochunk.expect("Indenter::write internal loop invariant failed");
+            written += self.write_eol_chunk(chunk)?;
+            ochunk = Some(nextchunk);
         }
 
-        Ok(wc.final_tally())
+        if let Some(chunk) = ochunk {
+            written += self.write_trailing_chunk(chunk)?;
+        }
+
+        Ok(written)
     }
 
     fn flush(&mut self) -> Result<()> {
-        self.0.flush()
+        self.f.flush()
+    }
+}
+
+impl<W: Write> Indenter<W> {
+    fn write_eol_chunk(&mut self, chunk: &[u8]) -> Result<usize> {
+        self._write_chunk(chunk, true)
+    }
+
+    fn write_trailing_chunk(&mut self, chunk: &[u8]) -> Result<usize> {
+        if chunk.len() > 0 {
+            self._write_chunk(chunk, false)
+        } else {
+            Ok(0)
+        }
+    }
+
+    fn _write_chunk(&mut self, chunk: &[u8], newline: bool) -> Result<usize> {
+        if self.linestart {
+            self.f.write_all("| ".as_bytes())?;
+        }
+        self.linestart = newline;
+
+        self.f.write_all(chunk)?;
+        if newline {
+            self.f.write_all(&[b'\n'])?;
+        }
+
+        Ok(chunk.len() + if newline { 1 } else { 0 })
     }
 }
 
 #[cfg(test)]
 impl<W> Indenter<W> {
     fn unwrap(self) -> W {
-        self.0
+        self.f
     }
 }
