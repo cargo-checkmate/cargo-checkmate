0e979ed7a0185feb2e1fcf24def91d79d477f841
nathan <nathan+dev@electriccoin.co>
2020-03-03 15:51:25 -0800

    Move phase::indenter::tests mod into indenter source file.

--- src/phase/indenter.rs
+++ src/phase/indenter.rs
@@ -1,6 +1,3 @@
-#[cfg(test)]
-mod tests;
-
 use std::io::{Result, Write};
 
 pub struct Indenter<W> {
@@ -70,8 +67,33 @@ impl<W: Write> Indenter<W> {
 }
 
 #[cfg(test)]
-impl<W> Indenter<W> {
-    fn unwrap(self) -> W {
-        self.f
+mod tests {
+    use super::Indenter;
+    use std::io::Write;
+
+    use test_case::test_case;
+
+    impl<W> Indenter<W> {
+        fn unwrap(self) -> W {
+            self.f
+        }
+    }
+
+    #[test_case(&String::new(), &String::new())]
+    #[test_case("blah", "| blah")]
+    #[test_case("foo\nbar", "| foo\n| bar")]
+    #[test_case("foo\nbar\n", "| foo\n| bar\n")]
+    #[test_case("foo\n\nbar", "| foo\n| \n| bar")]
+    fn transformation_tests(input: &str, expected: &str) {
+        for stride in 1..input.len() + 2 {
+            let mut id = Indenter::from(Vec::new());
+
+            for inchunk in input.as_bytes().chunks(stride) {
+                id.write_all(inchunk).unwrap();
+            }
+            let outvec = id.unwrap();
+            let actual = std::str::from_utf8(&outvec).unwrap();
+            assert_eq!(actual, expected, "stride {}", stride);
+        }
     }
 }
