47e628e87e6c186588fb6aa11a2a731ea25d2c62
nathan <nathan+dev@electriccoin.co>
2020-03-03 18:39:51 -0800

    Add a test failure injection point for manually testing phase failures.

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,11 @@ mod runner;
 fn main() -> std::io::Result<()> {
     runner::run(phases::PHASES)
 }
+
+#[test]
+fn test_fail_injector() {
+    use std::env;
+
+    let name = format!("{}-INJECT-TEST-FAILURE", env!("CARGO_PKG_NAME"));
+    assert!(std::env::var_os(name).is_none());
+}
