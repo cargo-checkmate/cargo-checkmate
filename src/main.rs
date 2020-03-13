e15b602e4bd19dc101875cf953e1bd32627fdb35
nathan <nathan+dev@electriccoin.co>
2020-03-13 16:55:45 -0700

    Move argument parsing into a mod.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![deny(warnings)]
 
+mod cli;
 mod phases;
 mod runner;
 mod subcommands;
@@ -10,24 +11,10 @@ mod tfi;
 const CMDNAME: &'static str = env!("CARGO_PKG_NAME");
 
 fn main() -> std::io::Result<()> {
-    let args: Vec<String> = std::env::args().collect();
-    let argrefs: Vec<&str> = args.iter().map(|s| s.as_str()).collect();
-    let subargs = if argrefs.get(1) == Some(&"checkmate") {
-        &argrefs[2..]
-    } else {
-        &argrefs[1..]
-    };
+    use crate::cli::{parse_args, Command::*};
 
-    match subargs {
-        &[] => runner::run(phases::PHASES),
-        &["audit"] => subcommands::audit(),
-        _ => {
-            use std::io::{Error, ErrorKind};
-
-            Err(Error::new(
-                ErrorKind::InvalidInput,
-                format!("Unknown args: {:?}", subargs),
-            ))
-        }
+    match parse_args(std::env::args())? {
+        Everything => runner::run(phases::PHASES),
+        Audit => subcommands::audit(),
     }
 }
