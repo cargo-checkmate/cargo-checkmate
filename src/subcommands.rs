a607cf7a14dab4d3236febf6b7bc248c4b2dbb5e
Nate Wilcox <nathan+dev@electriccoin.co>
2021-09-05 09:27:51 -0700

    Expire audit timestamp after 3 hours.

--- src/subcommands.rs
+++ src/subcommands.rs
@@ -1,5 +1,7 @@
 use std::path::Path;
-use std::time::SystemTime;
+use std::time::{Duration, SystemTime};
+
+const AUDIT_EXPIRATION_TIMEOUT_SECS: u64 = 60 * 60 * 3;
 
 pub fn cargo_builtin(args: &[&str]) -> std::io::Result<()> {
     use std::process::{exit, Command};
@@ -10,34 +12,53 @@ pub fn cargo_builtin(args: &[&str]) -> std::io::Result<()> {
 
 pub fn audit(force: bool) -> std::io::Result<()> {
     if force {
-        use abscissa_core::application::Application;
-        use cargo_audit::application::APPLICATION as AUDIT_APP;
-
-        Application::run(&AUDIT_APP, vec![String::from("audit")]);
-
-        Ok(())
+        force_audit()
     } else {
-        use std::path::PathBuf;
-        use std::process::Command;
+        audit_if_necessary()
+    }
+}
 
-        let stamp = PathBuf::from("target/audit.timestamp");
-        let stamptime = modified(&stamp)?;
-        let locktime = modified("Cargo.lock")?;
+fn force_audit() -> std::io::Result<()> {
+    use abscissa_core::application::Application;
+    use cargo_audit::application::APPLICATION as AUDIT_APP;
 
-        if stamptime > locktime {
-            println!("Skipped due to recent timestamp: {:?}", stamp);
-            Ok(())
-        } else {
-            let exe = std::env::current_exe()?;
-            let status = dbg!(Command::new(exe).arg("audit").arg("--force")).status()?;
+    Application::run(&AUDIT_APP, vec![String::from("audit")]);
 
-            if status.success() {
-                // Touch the timestamp path:
-                std::fs::File::create(stamp)?;
-            }
+    Ok(())
+}
 
-            std::process::exit(status.code().unwrap());
+fn audit_if_necessary() -> std::io::Result<()> {
+    use std::path::PathBuf;
+    use std::process::Command;
+
+    let stamp = PathBuf::from("target/audit.timestamp");
+    let stamptime = modified(&stamp)?;
+
+    let stampage = stamptime.elapsed().map_err(|e| {
+        use std::io::{Error, ErrorKind::Other};
+        Error::new(Other, format!("{:?}", e))
+    })?;
+
+    // Stamp is expired if we haven't done an audit in too long:
+    let expired = stampage >= audit_expiration_duration();
+
+    // Stamp is stale if Cargo.lock has been updated:
+    let stale = stamptime < modified("Cargo.lock")?;
+
+    if expired || stale {
+        let exe = std::env::current_exe()?;
+        let status = dbg!(Command::new(exe).arg("audit").arg("--force")).status()?;
+
+        if status.success() {
+            // Touch the timestamp path:
+            std::fs::File::create(stamp)?;
         }
+
+        std::process::exit(status.code().unwrap());
+    } else {
+        // stamp is recent and newer than lockfile:
+        println!("Skipped due to recent timestamp: {:?}", stamp);
+        Ok(())
     }
 }
 
@@ -48,4 +69,9 @@ fn modified<P: AsRef<Path>>(p: P) -> std::io::Result<SystemTime> {
     } else {
         Ok(SystemTime::UNIX_EPOCH)
     }
+}
+
+fn audit_expiration_duration() -> Duration {
+    // TODO: Should be const fn.
+    Duration::new(AUDIT_EXPIRATION_TIMEOUT_SECS, 0)
 }
