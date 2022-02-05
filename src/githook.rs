25d7322104c5c1d680a8ada0885f6baa9675e048
Nate Wilcox <nathan+dev@electriccoin.co>
2022-02-05 07:59:33 -0800

    Extract all generic code from `githook` into `srcbundle`.

--- src/githook.rs
+++ src/githook.rs
@@ -1,6 +1,6 @@
 use crate::executable::Executable;
 use crate::IOResult;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 /// git-hook support.
@@ -14,53 +14,16 @@ pub enum GitHook {
 
 impl Executable for GitHook {
     fn execute(&self) -> IOResult<()> {
+        use crate::srcbundle::{install, uninstall};
         use GitHook::*;
 
+        const HOOK_BODY: &[u8] = include_bytes!("githook-pre-commit.sh");
+        let dest = hook_path()?;
+
         match self {
-            Install => install(),
-            Uninstall => uninstall(),
+            Install => install("git-hook", &dest, HOOK_BODY, true),
+            Uninstall => uninstall("git-hook", &dest, HOOK_BODY),
         }
-    }
-}
-
-const HOOK_BODY: &[u8] = include_bytes!("githook-pre-commit.sh");
-
-fn install() -> IOResult<()> {
-    use crate::srcbundle::{open_if_non_existent, FileOrAlreadyExists::*};
-    use crate::CMDNAME;
-    use std::io::Write;
-
-    let hookpath = hook_path()?;
-    match open_if_non_existent(&hookpath)? {
-        File(mut f) => {
-            f.write_all(HOOK_BODY)?;
-            make_executable(f)?;
-            println!("{} git-hook installed: {:?}", CMDNAME, &hookpath);
-            Ok(())
-        }
-        AlreadyExists => {
-            if hook_contents_recognized(&hookpath)? {
-                println!("{} git-hook already installed: {:?}", CMDNAME, &hookpath);
-                Ok(())
-            } else {
-                unrecognized_hook_contents(&hookpath)
-            }
-        }
-    }
-}
-
-fn uninstall() -> IOResult<()> {
-    let hookpath = hook_path()?;
-
-    match hook_contents_recognized(&hookpath) {
-        Ok(true) => {
-            use crate::CMDNAME;
-            std::fs::remove_file(&hookpath)?;
-            println!("{} git-hook uninstalled: {:?}", CMDNAME, &hookpath);
-            Ok(())
-        }
-        Ok(false) => unrecognized_hook_contents(&hookpath),
-        Err(e) => Err(e),
     }
 }
 
@@ -71,26 +34,4 @@ fn hook_path() -> IOResult<PathBuf> {
 fn git_dir() -> IOResult<PathBuf> {
     let s = crate::git::run(&["rev-parse", "--git-dir"])?;
     Ok(PathBuf::from(s.trim_end()))
-}
-
-fn make_executable(f: std::fs::File) -> IOResult<()> {
-    use std::os::unix::fs::PermissionsExt;
-
-    let mut perms = f.metadata()?.permissions();
-    // Set user read/write perms on unix:
-    perms.set_mode(perms.mode() | 0o500);
-    f.set_permissions(perms)?;
-    Ok(())
-}
-
-fn hook_contents_recognized(hookpath: &Path) -> IOResult<bool> {
-    let contents = std::fs::read(&hookpath)?;
-    Ok(contents == HOOK_BODY)
-}
-
-fn unrecognized_hook_contents(hookpath: &Path) -> IOResult<()> {
-    use crate::{ioerror, CMDNAME};
-
-    println!("{} unrecognized git-hook: {:?}", CMDNAME, &hookpath);
-    Err(ioerror!("Unrecongized git-hook: {:?}", &hookpath))
 }
--- src/srcbundle.rs
+++ src/srcbundle.rs
@@ -1,13 +1,50 @@
 use crate::IOResult;
 use std::path::Path;
 
+pub fn install(name: &str, dest: &Path, contents: &[u8], executable: bool) -> IOResult<()> {
+    use crate::CMDNAME;
+    use FileOrAlreadyExists::*;
+
+    match open_if_non_existent(dest)? {
+        File(mut f) => {
+            use std::io::Write;
+
+            f.write_all(contents)?;
+            if executable {
+                make_executable(f)?;
+            }
+            println!("{} {} installed: {:?}", CMDNAME, name, dest);
+            Ok(())
+        }
+        AlreadyExists => {
+            if contents_recognized(dest, contents)? {
+                println!("{} {} already installed: {:?}", CMDNAME, name, dest);
+                Ok(())
+            } else {
+                unrecognized_contents(name, dest)
+            }
+        }
+    }
+}
+
+pub fn uninstall(name: &str, dest: &Path, contents: &[u8]) -> IOResult<()> {
+    if contents_recognized(dest, contents)? {
+        use crate::CMDNAME;
+        std::fs::remove_file(dest)?;
+        println!("{} {} uninstalled: {:?}", CMDNAME, name, dest);
+        Ok(())
+    } else {
+        unrecognized_contents(name, dest)
+    }
+}
+
 #[derive(Debug)]
-pub enum FileOrAlreadyExists {
+enum FileOrAlreadyExists {
     File(std::fs::File),
     AlreadyExists,
 }
 
-pub fn open_if_non_existent(path: &Path) -> IOResult<FileOrAlreadyExists> {
+fn open_if_non_existent(path: &Path) -> IOResult<FileOrAlreadyExists> {
     use FileOrAlreadyExists::*;
 
     let openres = std::fs::OpenOptions::new()
@@ -22,4 +59,26 @@ pub fn open_if_non_existent(path: &Path) -> IOResult<FileOrAlreadyExists> {
             _ => Err(e),
         },
     }
+}
+
+fn make_executable(f: std::fs::File) -> IOResult<()> {
+    use std::os::unix::fs::PermissionsExt;
+
+    let mut perms = f.metadata()?.permissions();
+    // Set user read/write perms on unix:
+    perms.set_mode(perms.mode() | 0o500);
+    f.set_permissions(perms)?;
+    Ok(())
+}
+
+fn contents_recognized(dest: &Path, contents: &[u8]) -> IOResult<bool> {
+    let found = std::fs::read(dest)?;
+    Ok(found == contents)
+}
+
+fn unrecognized_contents(name: &str, dest: &Path) -> IOResult<()> {
+    use crate::{ioerror, CMDNAME};
+
+    println!("{} unrecognized {}: {:?}", CMDNAME, name, dest);
+    Err(ioerror!("Unrecongized {}: {:?}", name, dest))
 }
