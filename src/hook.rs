e5c4c6848c73ef8d4f96d84e4298f41ce6cbd704
Nate Wilcox <nathan+dev@electriccoin.co>
2022-03-04 14:48:43 -0800

    Improve the --help output for `… hook install` & `… uninstall`.

--- src/hook.rs
+++ src/hook.rs
@@ -6,12 +6,20 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 pub enum Hook {
     /// install repository hooks
-    Install(HookType),
+    Install(HookTypeOption),
     /// uninstall repository hooks
-    Uninstall(HookType),
+    Uninstall(HookTypeOption),
 }
 
-/// manage repository hooks.
+/// hook type option
+#[derive(Debug, StructOpt)]
+pub struct HookTypeOption {
+    /// Hook type: all, git, or github-ci
+    #[structopt(default_value)]
+    hook_type: HookType,
+}
+
+/// hook type
 #[derive(Debug, StructOpt)]
 pub enum HookType {
     /// all hooks
@@ -22,8 +30,55 @@ pub enum HookType {
     GithubCI,
 }
 
+impl Default for HookType {
+    fn default() -> HookType {
+        HookType::All
+    }
+}
+
 impl Executable for Hook {
     fn execute(&self) -> IOResult<()> {
-        todo!();
+        use Hook::*;
+
+        match self {
+            Install(HookTypeOption { hook_type }) => todo!("install {:?}", hook_type),
+            Uninstall(HookTypeOption { hook_type }) => todo!("uninstall {:?}", hook_type),
+        }
+    }
+}
+
+// TODO: structopt/clap already knows how to format these, can we reuse that?
+impl std::fmt::Display for HookType {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        use HookType::*;
+
+        write!(
+            f,
+            "{}",
+            match self {
+                All => "all",
+                Git => "git",
+                GithubCI => "github-ci",
+            }
+        )
+    }
+}
+
+// TODO: structopt/clap already knows how to format these, can we reuse that?
+impl std::str::FromStr for HookType {
+    type Err = String;
+
+    fn from_str(src: &str) -> Result<HookType, String> {
+        use HookType::*;
+
+        if src == "all" {
+            Ok(All)
+        } else if src == "git" {
+            Ok(Git)
+        } else if src == "github-ci" {
+            Ok(GithubCI)
+        } else {
+            Err(format!("Unknown hook: {:?}", src))
+        }
     }
 }
