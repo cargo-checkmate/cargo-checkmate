45cd070aa112f6a35f9711729c6ebc4a9e80ce2b
Nate Wilcox <nejucomo+dev@gmail.com>
2023-12-03 11:35:09 -0700

    flake.nix: include transitive runtime dependencies to work "out of the box".

--- flake.nix
+++ flake.nix
@@ -1,10 +1,19 @@
 # A nix flake for `cargo-checkmate`.
 #
-# Nix users can install `cargo-checkmate` with:
+# Nix users can start a shell w/ checkmate available with:
+#
+# ```
+# $ nix shell 'github:cargo-checkmate/cargo-checkmate'
+# ```
+#
+# -or they can install it for their user profile with:
 #
 # ```
 # $ nix profile install 'github:cargo-checkmate/cargo-checkmate'
 # ```
+#
+# Note: this nix flake includes all transitive runtime dependencies,
+# such as cargo and gcc to work "out of the box".
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.nci.url = "github:yusdacra/nix-cargo-integration";
@@ -31,11 +40,25 @@
         # shorthand for accessing this crate's outputs
         # you can access crate outputs under `config.nci.outputs.<crate name>` (see documentation)
         crateOutputs = config.nci.outputs."cargo-checkmate";
+        cargo-checkmate-pkg = crateOutputs.packages.release;
+
+        # We need the rust toolchain as a runtime dependency:
+        wrapped-pkg = pkgs.symlinkJoin {
+          name = "${cargo-checkmate-pkg.name}-with-bundled-cargo";
+          paths = [
+            pkgs.gcc
+            pkgs.cargo
+            pkgs.clippy
+            pkgs.rustfmt
+            cargo-checkmate-pkg
+          ];
+        };
       in {
         # export the crate devshell as the default devshell
         devShells.default = crateOutputs.devShell;
-        # export the release package of the crate as default package
-        packages.default = crateOutputs.packages.release;
+        packages.default = wrapped-pkg;
+        # Expose the unwrapped package if some consumers need it:
+        packages.unwrapped = cargo-checkmate-pkg;
       };
     };
 }
